a463482b0fe04dcd43adc0b7311f4110f90ab59f
DavidGdoT <david.guajardot@uanl.edu.mx>
2026-05-09 00:56:57 -0600

    Update data_api.py
    Agregue que se pueda elegir la base para las conversiones (no es final)

--- data_api.py
+++ data_api.py
@@ -2,14 +2,13 @@ import requests
 import json
 
 def obtener_datos():
-    url = 'https://v6.exchangerate-api.com/v6/b6d737f0e4b316d91afd4e28/latest/USD'
+    base = str(input('agrega la base a utilizar '))
+    url = 'https://v6.exchangerate-api.com/v6/b6d737f0e4b316d91afd4e28/latest/'+str(base) 
     reponse = requests.get(url)
     data = json.loads(reponse.text)
     return data
 
 if __name__ == "__main__":
     datos = obtener_datos()
-    print(datos)
     monedas = datos.get("conversion_rates")
-    print(monedas)
     
