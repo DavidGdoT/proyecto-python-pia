9ae02079242d6c2d1ed961c1f9bc0b50c211347e
DavidGdoT <david.guajardot@uanl.edu.mx>
2026-05-09 00:04:14 -0600

    Update conversiones.py
    Mejore el Ciclo while para evitar duplicados y listas vacias y agregue una comparación con las monedas elegidas

--- conversiones.py
+++ conversiones.py
@@ -1,17 +1,30 @@
 from api_datos import obtener_datos
+import matplotlib.pyplot as plt
 
 datos = obtener_datos()
 monedas = datos.get('conversion_rates')
 lista_mon = []
+mon_values = []
 
 while True:
     mon = str(input('nombre de abreviado de la moneda (escribe "stop" para terminar) '))
-    if mon in monedas.keys():
-        lista_mon.append(mon)
-    elif mon == 'stop':
-        break
-    else:
-        print('moneda no existente')
-
-for _ in lista_mon:
-    print('1 USD =', monedas.get(_), _)
+    if mon == 'stop':
+        if len(lista_mon) == 0:
+            print('agregar monedas')
+        else:
+            break
+    elif mon in lista_mon:
+        print('moneda ya agregada')
+    elif mon not in lista_mon:
+        if mon in monedas.keys():
+            lista_mon.append(mon)
+        else:
+            print("moneda no existente")
+    
+for nombre in lista_mon:
+    mon_values.append(monedas.get(nombre))
+    print('1 USD =', monedas.get(nombre), nombre)
+    
+#para texto más extenso crear una bibloteca con las moendas elegidas
+plt.plot(lista_mon, mon_values, marker="p")
+plt.savefig("con.png")
