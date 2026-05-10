ff799bb659ab6d13c2a5fa242f83bdd2f65361ff
DavidGdoT <david.guajardot@uanl.edu.mx>
2026-05-09 22:29:55 -0600

    Update data_api.py
    Ajustes para adaptarse al cambio de base

--- data_api.py
+++ data_api.py
@@ -2,13 +2,11 @@ import requests
 import json
 
 def obtener_datos():
-    base = str(input('agrega la base a utilizar '))
-    url = 'https://v6.exchangerate-api.com/v6/b6d737f0e4b316d91afd4e28/latest/'+str(base) 
+    url = 'https://v6.exchangerate-api.com/v6/b6d737f0e4b316d91afd4e28/latest/USD'
     reponse = requests.get(url)
     data = json.loads(reponse.text)
     return data
-
+    
 if __name__ == "__main__":
     datos = obtener_datos()
     monedas = datos.get("conversion_rates")
-    
